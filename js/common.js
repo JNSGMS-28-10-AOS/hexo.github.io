b0aed5ed9704f4f3ad139be24c58c235fa4fce1e
JNSGMS-28-10-AOS <yz3313844163@163.com>
2024-11-08 21:15:17 +0800

    添加全部文件

---
+++ js/common.js
@@ -0,0 +1,8 @@
+// 匿名函数
+$(document).ready(() => {
+    // 滚动条滚动到顶部，不要动画
+    // $("#header").load("header.html");
+    // $("#footer").load("footer.html");
+    $("#header").load("https://yubac.github.io/2810security.github.io/header.html");
+    $("#footer").load("https://yubac.github.io/2810security.github.io/footer.html");
+});
